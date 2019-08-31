c0779317c9aa467ba6cef403b099658caa090d9c
Tutoriais <Tutoriais@Mac-mini-de-Felippe.local>
2019-08-31 17:15:43 -0300

    #001

---
+++ Firsrt App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firsrt App
+//
+//  Created by Tutoriais on 31/08/19.
+//  Copyright © 2019 Awesome Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
